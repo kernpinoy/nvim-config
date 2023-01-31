dd1151b664e3463b0811b1c567c2a93654da07fa
Kern Jacobo <jacobokern5@gmail.com>
2023-01-31 15:57:37 +0800

    Added nvim-autopairs

--- lua/kernpinoy/packer.lua
+++ lua/kernpinoy/packer.lua
@@ -4,70 +4,75 @@
 vim.cmd [[packadd packer.nvim]]
 
 return require('packer').startup(function(use)
-  -- Packer can manage itself
-  use 'wbthomason/packer.nvim'
+    -- Packer can manage itself
+    use 'wbthomason/packer.nvim'
 
-  use {
-  	'nvim-telescope/telescope.nvim', tag = '0.1.1',
-	-- or                            , branch = '0.1.x',
-  	requires = { {'nvim-lua/plenary.nvim'} }
-  }
-  
-  -- Change colorscheme
-  use ({ 
-	  "catppuccin/nvim",
-	  as = "catppuccin",
-	  config = function()
-		  vim.cmd("colorscheme catppuccin-mocha")
-	  end
+    use {
+        'nvim-telescope/telescope.nvim', tag = '0.1.1',
+        -- or                            , branch = '0.1.x',
+        requires = { {'nvim-lua/plenary.nvim'} }
+    }
 
-  })
+    -- Change colorscheme
+    use ({ 
+        "catppuccin/nvim",
+        as = "catppuccin",
+        config = function()
+            vim.cmd("colorscheme catppuccin-mocha")
+        end
 
-  -- Treesitter
-  use({'nvim-treesitter/nvim-treesitter', run = ':TSUpdate'})
-  
-  -- Undotree
-  use('mbbill/undotree')
-  
-  -- Git integrations
-  use('tpope/vim-fugitive')
+    })
 
-  -- LSP 
-  use {
-  	'VonHeikemen/lsp-zero.nvim',
-  	branch = 'v1.x',
-  	requires = {
-    	-- LSP Support
-    	{'neovim/nvim-lspconfig'},             -- Required
-    	{'williamboman/mason.nvim'},           -- Optional
-    	{'williamboman/mason-lspconfig.nvim'}, -- Optional
+    -- Treesitter
+    use({'nvim-treesitter/nvim-treesitter', run = ':TSUpdate'})
 
-    	-- Autocompletion
-    	{'hrsh7th/nvim-cmp'},         -- Required
-    	{'hrsh7th/cmp-nvim-lsp'},     -- Required
-    	{'hrsh7th/cmp-buffer'},       -- Optional
-    	{'hrsh7th/cmp-path'},         -- Optional
-    	{'saadparwaiz1/cmp_luasnip'}, -- Optional
-    	{'hrsh7th/cmp-nvim-lua'},     -- Optional
+    -- Undotree
+    use('mbbill/undotree')
 
-    	-- Snippets
-    	{'L3MON4D3/LuaSnip'},             -- Required
-    	{'rafamadriz/friendly-snippets'}, -- Optional
-  	}
-  }
+    -- Git integrations
+    use('tpope/vim-fugitive')
 
-  -- UI stuffs
-  use 'nvim-tree/nvim-web-devicons'
-  use {'romgrk/barbar.nvim', wants = 'nvim-web-devicons'}
+    -- LSP 
+    use {
+        'VonHeikemen/lsp-zero.nvim',
+        branch = 'v1.x',
+        requires = {
+            -- LSP Support
+            {'neovim/nvim-lspconfig'},             -- Required
+            {'williamboman/mason.nvim'},           -- Optional
+            {'williamboman/mason-lspconfig.nvim'}, -- Optional
 
-  -- Status line
-  use 'feline-nvim/feline.nvim'
+            -- Autocompletion
+            {'hrsh7th/nvim-cmp'},         -- Required
+            {'hrsh7th/cmp-nvim-lsp'},     -- Required
+            {'hrsh7th/cmp-buffer'},       -- Optional
+            {'hrsh7th/cmp-path'},         -- Optional
+            {'saadparwaiz1/cmp_luasnip'}, -- Optional
+            {'hrsh7th/cmp-nvim-lua'},     -- Optional
 
-  -- Toggle terminal
-  use {
-      "akinsho/toggleterm.nvim", 
-      tag = '*', config = function()
-          require("toggleterm").setup()
-      end
-  }
+            -- Snippets
+            {'L3MON4D3/LuaSnip'},             -- Required
+            {'rafamadriz/friendly-snippets'}, -- Optional
+        }
+    }
+
+    -- UI stuffs
+    use 'nvim-tree/nvim-web-devicons'
+    use {'romgrk/barbar.nvim', wants = 'nvim-web-devicons'}
+
+    -- Status line
+    use 'feline-nvim/feline.nvim'
+
+    -- Toggle terminal
+    use {
+        "akinsho/toggleterm.nvim", 
+        tag = '*', config = function()
+            require("toggleterm").setup()
+        end
+    }
+
+    use {
+        "windwp/nvim-autopairs",
+        config = function() require("nvim-autopairs").setup {} end
+    }
 end)
--- plugin/packer_compiled.lua
+++ plugin/packer_compiled.lua
@@ -141,6 +141,12 @@ _G.packer_plugins = {
     path = "/home/pam/.local/share/nvim/site/pack/packer/start/mason.nvim",
     url = "https://github.com/williamboman/mason.nvim"
   },
+  ["nvim-autopairs"] = {
+    config = { "\27LJ\2\n@\0\0\3\0\3\0\a6\0\0\0'\2\1\0B\0\2\0029\0\2\0004\2\0\0B\0\2\1K\0\1\0\nsetup\19nvim-autopairs\frequire\0" },
+    loaded = true,
+    path = "/home/pam/.local/share/nvim/site/pack/packer/start/nvim-autopairs",
+    url = "https://github.com/windwp/nvim-autopairs"
+  },
   ["nvim-cmp"] = {
     loaded = true,
     path = "/home/pam/.local/share/nvim/site/pack/packer/start/nvim-cmp",
@@ -195,14 +201,18 @@ _G.packer_plugins = {
 }
 
 time([[Defining packer_plugins]], false)
--- Config for: toggleterm.nvim
-time([[Config for toggleterm.nvim]], true)
-try_loadstring("\27LJ\2\n8\0\0\3\0\3\0\0066\0\0\0'\2\1\0B\0\2\0029\0\2\0B\0\1\1K\0\1\0\nsetup\15toggleterm\frequire\0", "config", "toggleterm.nvim")
-time([[Config for toggleterm.nvim]], false)
+-- Config for: nvim-autopairs
+time([[Config for nvim-autopairs]], true)
+try_loadstring("\27LJ\2\n@\0\0\3\0\3\0\a6\0\0\0'\2\1\0B\0\2\0029\0\2\0004\2\0\0B\0\2\1K\0\1\0\nsetup\19nvim-autopairs\frequire\0", "config", "nvim-autopairs")
+time([[Config for nvim-autopairs]], false)
 -- Config for: catppuccin
 time([[Config for catppuccin]], true)
 try_loadstring("\27LJ\2\n@\0\0\3\0\3\0\0056\0\0\0009\0\1\0'\2\2\0B\0\2\1K\0\1\0!colorscheme catppuccin-mocha\bcmd\bvim\0", "config", "catppuccin")
 time([[Config for catppuccin]], false)
+-- Config for: toggleterm.nvim
+time([[Config for toggleterm.nvim]], true)
+try_loadstring("\27LJ\2\n8\0\0\3\0\3\0\0066\0\0\0'\2\1\0B\0\2\0029\0\2\0B\0\1\1K\0\1\0\nsetup\15toggleterm\frequire\0", "config", "toggleterm.nvim")
+time([[Config for toggleterm.nvim]], false)
 
 _G._packer.inside_compile = false
 if _G._packer.needs_bufread == true then
