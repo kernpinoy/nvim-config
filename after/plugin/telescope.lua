3b5f437259f0227929ea1b8ffa6391715254f628
Kern Jacobo <jacobokern5@gmail.com>
2023-01-31 16:06:00 +0800

    Fixed telescope config error

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -1,7 +1,4 @@
 local builtin = require('telescope.builtin')
-    if (counter == 1) {
-
-    }
 
 vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
 vim.keymap.set('n', '<C-p>', builtin.git_files, {})
